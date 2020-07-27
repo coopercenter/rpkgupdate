1e3183ab6561f1b63eb7bfdc512bf04ae4c56036
dhillii <dh5dn@virginia.edu>
2020-07-27 02:46:17 -0400

    comments

--- rpkgupdate.sh
+++ rpkgupdate.sh
@@ -1,15 +1,15 @@
 
-sudo apt install -y build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev 
+#sudo apt install -y build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev 
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+#sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
-sudo add-apt-repository sudo add-apt-repository 'deb ppa:ubuntugis/ppa'
+#sudo add-apt-repository sudo add-apt-repository 'deb ppa:ubuntugis/ppa'
 
-sudo apt update -y
+#sudo apt update -y
 
-sudo apt-get install -y libssl-dev libcurl4-openssl-dev libudunits2-dev r-base
+#sudo apt-get install -y libssl-dev libcurl4-openssl-dev libudunits2-dev r-base
 
 python /src/rpkgupdate.py
 
--- src/rpkgupdate.py
+++ src/rpkgupdate.py
@@ -26,11 +26,11 @@ def setup():
     password = stdiomask.getpass("Github Password: ")
 
     try:
-        g = Github(user, password) 
+        g = Github(user, password)
     except:
         print("Username or Password incorrect.")
         return
-    
+
     # get cooper center organization
     try:
         org = g.get_organization("coopercenter")
@@ -43,6 +43,12 @@ def setup():
 
     return org_repos
 
+"""
+@param set r_packages - the set of r packages that we find
+@param FileContents file - the R or Rmd file
+@brief gets all substrings between library() and require() token parentheses and
+adds those dependency strings to the set of r packages
+"""
 def GetDeps(r_packages, file):
     contents = str(file.decoded_content)
     pattern1 = r"library\((.*?)\)"
@@ -58,9 +64,10 @@ def main():
     r_pkgs = set()
     content_files = []
     r_files = []
+
+    # this is needed for something R related
     utils = rpackages.importr('utils')
 
-    
 
     # get all repos from setup
     repos = setup()
@@ -76,7 +83,7 @@ def main():
             if repo.language == 'R':
                 r_repos.append(repo)
 
-        # get all files from all R repos
+        # get all files from all R repos recursively
         for repo in r_repos:
             spinner1.next()
             contents = repo.get_contents("")
@@ -88,9 +95,6 @@ def main():
                 else:
                     content_files.append(file_content)
 
-        
-        
-
         # get all r and rmd files
         for file in content_files:
             spinner1.next()
@@ -99,6 +103,8 @@ def main():
         spinner1.finish()
 
         spinner2 = Spinner('Finding Dependencies...')
+
+        # Get deps from files
         for file in r_files:
             spinner2.next()
             GetDeps(r_pkgs, file)
@@ -106,17 +112,20 @@ def main():
         spinner2.finish()
         print("\nDependencies Collected. Installing!")
 
-        pkgs_to_install = [x for x in r_pkgs if not rpackages.isinstalled(x)]
-        
-        '''try: PUT IN BASH SCRIPT
-            check_call(['sudo apt-get', 'install', '-y', 'libssl-dev libcurl4-openssl-dev libudunits2-dev'], stdout=open(os.devnull,'wb'))
-        except CalledProcessError as e:
-            print(e.output)
-        '''
-        
+        # getting a little jank here but it works. Might improve later...
         pkgs_to_install.append('devtools')
+        pkgs_to_install.append('pkgconfig')
+        pkgs_to_install.append('latticeExtra')
+        pkgs_to_install.append('cowplot')
+        pkgs_to_install.append('rstatix')
+        pkgs_to_install.append('car')
+        pkgs_to_install.append('XML')
+        pkgs_to_install.append('scholar')
         pkgs_to_install.append('dependencies = TRUE')
-        if len(pkgs_to_install) > 0:
+
+        pkgs_to_install += [x for x in r_pkgs if not rpackages.isinstalled(x)]
+
+        if len(pkgs_to_install) > 9:
             utils.install_packages(StrVector(pkgs_to_install))
         else:
             print("All packages up to date!")
@@ -124,7 +133,7 @@ def main():
 
     else:
         exit()
-    
+
 
 
 if __name__ == "__main__":
